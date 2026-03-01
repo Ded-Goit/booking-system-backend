13250a013bcd163305fbc8e6e33aed17416dd256
Andriy Romanov <mgm.agro04@gmail.com>
2026-03-01 20:51:04 +0200

    start project

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// 🔥 ОБОВʼЯЗКОВО має бути це
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
